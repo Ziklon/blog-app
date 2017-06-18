273a8d12d5e77e1a8b907c1e7a8b80f81df48f7d
Wilber Torres <winftc@gmail.com>
2017-06-18 18:18:28 -0500

    adding join column to post

--- blog-note/src/main/java/com/darkbit/blog/post/Post.java
+++ blog-note/src/main/java/com/darkbit/blog/post/Post.java
@@ -4,9 +4,14 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.darkbit.blog.user.User;
 
 import lombok.Data;
 
@@ -16,22 +21,25 @@ import lombok.Data;
  *
  */
 @Entity
-@Table(name="posts")
+@Table(name = "posts")
 @Data
-public class Post implements Serializable{
+public class Post implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7532555595122053875L;
-	@Id 
+	@Id
 	@GeneratedValue
 	private Long id;
-	@Column(name="title")
+	@Column(name = "title")
 	private String title;
-	@Column(name="content")
+	@Column(name = "content")
 	private String content;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 	
 	
-	
+
 }
